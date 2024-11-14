8dc528ce8bb0e31ff8c7d87301719dabec84fdf3
kavindumal <kavindu11250403@gmail.com>
2024-11-14 17:45:38 +0530

    AppUtil userId generate | AppUtil image to 64Base convert

--- src/main/java/lk/ijse/green_shadow_pvt_ltdbackend/util/AppUtil.java
+++ src/main/java/lk/ijse/green_shadow_pvt_ltdbackend/util/AppUtil.java
@@ -1,4 +1,24 @@
 package lk.ijse.green_shadow_pvt_ltdbackend.util;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Base64;
+import java.util.UUID;
+
 public class AppUtil {
+
+    public static String createUserId(){
+        return "USER-"+ UUID.randomUUID();
+    }
+
+    public static String toBase64ProfilePic(MultipartFile profilePic){
+        String proPicBase64 = null;
+        try {
+            byte [] proPicBytes = profilePic.getBytes();
+            proPicBase64 =  Base64.getEncoder().encodeToString(proPicBytes);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return proPicBase64;
+    }
 }
