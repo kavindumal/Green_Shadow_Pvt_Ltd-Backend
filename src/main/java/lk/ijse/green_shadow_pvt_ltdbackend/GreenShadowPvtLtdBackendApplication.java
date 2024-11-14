ff942fdfc780c3595d0ed83d7feb8f6086b492a0
kavindumal <kavindu11250403@gmail.com>
2024-11-14 17:23:36 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/green_shadow_pvt_ltdbackend/GreenShadowPvtLtdBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.green_shadow_pvt_ltdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShadowPvtLtdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenShadowPvtLtdBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/green_shadow_pvt_ltdbackend/GreenShadowPvtLtdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.green_shadow_pvt_ltdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowPvtLtdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
